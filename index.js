2365a1137e037a855523c23b18a199fda185f011
lisapeters13 <112504985+lisapeters13@users.noreply.github.com>
2024-02-19 18:37:02 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function add() {
+    return "something";
+  }
+  function add(a, b) {
+    return a + b;
+  }function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+  
